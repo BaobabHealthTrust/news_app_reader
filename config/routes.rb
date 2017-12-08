c3f916d3fdbc14afc127ddde5356b9fb3e4c564a
jmziya <jacobmziya@gmail.com>
2017-12-08 11:46:51 +0200

    Adding input source from BHT.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 	
+  resources :news
 	root 'main#index'
 	
 	# ============= News PlugIn routes =======================
--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,41 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "category", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "name",        limit: 100, null: false
+    t.string "description",             null: false
+    t.date   "date",                    null: false
+  end
+
+  create_table "media", primary_key: "media_id", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "news_id",                         null: false
+    t.string  "filename",     limit: 100
+    t.string  "content_type", limit: 100
+    t.binary  "data",         limit: 4294967295
+  end
+
+  create_table "news", primary_key: "news_id", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "title",                                                              null: false
+    t.text     "body",       limit: 4294967295,                                      null: false
+    t.string   "category",   limit: 60,                                              null: false
+    t.date     "date",                                                               null: false
+    t.datetime "created_at",                    default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+  create_table "tracker", primary_key: "tracker_id", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "news_id",                                                     null: false
+    t.string   "ip_address", limit: 20,                                       null: false
+    t.string   "category",   limit: 100,                                      null: false
+    t.date     "date",                                                        null: false
+    t.datetime "created_at",             default: -> { "CURRENT_TIMESTAMP" }, null: false
+  end
+
+  create_table "user", primary_key: "user_id", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string "fname",    limit: 100, null: false
+    t.string "lname",    limit: 100, null: false
+    t.string "username", limit: 100, null: false
+    t.string "password", limit: 100, null: false
+    t.date   "date"
+  end
+
 end
